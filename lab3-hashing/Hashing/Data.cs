ffa9f427d21c826eb35237fe76e53c4d0540c86b
itiredofemotions <davydenko.md@students.dvfu.ru>
2023-02-13 11:15:29 +1000

    completed

---
+++ lab3-hashing/Hashing/Data.cs
@@ -0,0 +1,32 @@
+﻿namespace Hashing;
+
+public class Data
+{
+    public List<List<Game>> table = new List<List<Game>>();
+
+    public Data(int size)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            table.Add(new List<Game>());
+        }
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < table.Count; i++)
+            if (table[i].Count != 0)
+            {
+                int h = i;
+                string tmp = "";
+                for (int j = 0; j < table[h].Count; ++j)
+                {
+                    if (table[h][j].Id == i)
+                    {
+                        tmp += table[h][j].Key + " -> ";
+                    }
+                }
+                Console.WriteLine(i + ". " + tmp);
+            }
+    }
+}
